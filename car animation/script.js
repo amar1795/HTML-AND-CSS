c5aaebbcfe49ae69818181bff4caa78f1caba6ca
Amar <jeetamar496@gmail.com>
2023-08-24 20:39:46 +0530

    basic styling done

--- car animation/script.js
+++ car animation/script.js
@@ -1,3 +1,7 @@
 const audio= new Audio('sound.mp3')
 audio.loop=true;
-audio.play();
+audio.play()
+console.log("audio is playing")
+// audio by default will not play on google chrome because of the policy , it is the browser issue that the video is not playing 
+
+
