eebfc4a55cc995b1a49efd6ae3e9b0b1ee9879e3
Amar <jeetamar496@gmail.com>
2023-08-27 01:16:24 +0530

    snake moveing and food function added

--- snake game in javascript/script.js
+++ snake game in javascript/script.js
@@ -19,7 +19,7 @@ let food={
 
 function main(ctime) {
     // for animating with proper frames , not using set timeout as it will not give smooth animations
-
+    // console.log(ctime)
     window.requestAnimationFrame(main);
     if((ctime- lastPaintTime)/1000 < 1/speed)
     {
@@ -29,22 +29,73 @@ function main(ctime) {
     gameEngine();   
 }
 
+function iscollide() {
+    
+}
 
 
 function gameEngine() {
+    // case : when snake collides   
+    if(iscollide(snakeBody))
+    {
+        Gameovermusic.play();
+        bgmusic.pause();
+        // resetting the snake position
+        inputdir={x:0,y:0};
+        alert('Game Over, press any key to play again')
+        snakeBody=[{
+            x:14,y:15
+        }]
+        bgmusic.play();
+        score=0;       
+    }
+
+    if(snakeBody[0].y==food.y && snakeBody[0].x==food.x)
+    {
+        // body will be shifted to one block ahead after eating the food
+        snakeBody.unshift({x:snakeBody[0].x+inputdir.x ,y:snakeBody[0].y+inputdir.y})
+        let a=2;
+        let b=16;
+        // to generate random numbers the below method is used 
+        food={x:Math.round(a+(b-a)*Math.random()),y:Math.round(a+(b-a)*Math.random())}
+    }
+
+    // when the snake moves
+
+    for(let i=snakeBody.length-2;i>=0;i--)
+    {
+        // using destructuring because 
+        snakeBody[i+1]={...snakeBody[i]};
+    }
+
+        snakeBody[0].x+=inputdir.x;
+        snakeBody[0].y+=inputdir.y;
+        
 
     board.innerHTML="";
     snakeBody.forEach((e,index)=>{
         snakeelement=document.createElement('div');
         snakeelement.style.gridRowStart=e.y;
         snakeelement.style.gridColumnStart=e.x;
-        snakeelement.classList.add('head');
-        board.appendChild(snakeelement);     
-    })
+        
+        if(index===0)
+        {
+            // add the head of the snake at the begining when the index is 0 to the board
+            snakeelement.classList.add('head');
+        }
+        else
+        {
+            // further keep adding the body when the snake eats the food each time 
+            snakeelement.classList.add('snakebody');
+            
+        }
+        // add the complete snake to the board 
+        board.appendChild(snakeelement); 
+                    });
 
         foodelement=document.createElement('div');
-        foodelement.style.gridRowStart=e.y;
-        foodelement.style.gridColumnStart=e.x;
+        foodelement.style.gridRowStart=food.y;
+        foodelement.style.gridColumnStart=food.x;
         foodelement.classList.add('food');
         board.appendChild(foodelement);     
     
@@ -66,5 +117,36 @@ function gameEngine() {
 
 
 
+// main logic
+window.requestAnimationFrame(main);
+window.addEventListener('keydown',e=>{
+    inputdir={x:0,y:1}
+    movemusic.play();
+    switch (e.key) {
+        case "ArrowUp":
+            inputdir.x=0;
+            inputdir.y=-1;
+            
+            console.log("arrowup")
+            break;
+        case "ArrowDown":
+            inputdir.x=0;
+            inputdir.y=1;
+                console.log("arrowdown")
+                break;
+        case "ArrowLeft":
+            inputdir.x=-1;
+            inputdir.y=0;
+                console.log("arrowleft")
+                break;
+        case "ArrowRight":
+            inputdir.x=1;
+            inputdir.y=0;
+            console.log("arrowright")
+            break;
+    
+        default:
+            break;
+    }
 
-Window.requestAnimationFrame(main);
+})
