cb713a48f4068651d3dbc898211491675d7452b7
Amar <jeetamar496@gmail.com>
2023-08-30 00:17:32 +0530

    multiple save image button error corrected

--- QR code generator/script.js
+++ QR code generator/script.js
@@ -5,6 +5,7 @@ const qr=document.getElementById("qrcode");
 const onGenerateSubmit=(e)=>{
     e.preventDefault();
     clearUI();
+
     const url=document.getElementById('url').value;
     const size=document.getElementById('size').value;
     
@@ -37,19 +38,22 @@ const generateQRcode=(url,size)=>{
 
 }
 
-const createSavebtn=(saveUrl)=>{
-    const link=document.createElement('a');
-    link.id="save-link";
-    link.classList="bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5";
-    link.href=saveUrl;
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement("a");
+    link.id = "save-link";
+    link.classList =
+      'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+    link.innerHTML = "Save Image";
+  
+    link.href = saveUrl;
     link.download = "qrcode.png";
-
+  
     document.getElementById("generated").appendChild(link);
-
-}
-
+  };
 const clearUI=()=>{
 qr.innerHTML="";
+link = document.createElement("a");
+document.getElementById("generated").innerHTML="";
 }
 
 const showSpinner=()=>{
