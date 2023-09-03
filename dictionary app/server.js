b0efff8154a4c55c0b72d660c73ae27404c3e41f
Amar <jeetamar496@gmail.com>
2023-09-04 03:16:52 +0530

    cards added for displaying the dictionary

--- dictionary app/server.js
+++ dictionary app/server.js
@@ -9,21 +9,19 @@ const path = require('path');
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,"./","index.html"))
-  
+    res.sendFile(path.join(__dirname,"./","index.html")) 
 })
 
-app.get('/searchword', (req, res) => {
-  res.send('Hello World!')
+
+app.get('/searchword', async(req, res) => {
+
   
 // console.log(req.params)
-const dictionary=async()=>{
-
 
 const options = {
   method: 'GET',
   url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
-  params: {term: 'tech'},
+  params: {term: req.query.term},
   headers: {
     'X-RapidAPI-Key': '5993f902b8mshc12c46803594a66p12c663jsncc7d851ba210',
     'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
@@ -32,13 +30,13 @@ const options = {
 
 try {
 	const response = await axios.request(options);
-	// console.log(response.data);
+	console.log(response.data);
+    res.json(response.data)
 } catch (error) {
 	console.error(error);
 }
-  }
 
-  dictionary();
+
 })
 
 app.listen(port, () => {
