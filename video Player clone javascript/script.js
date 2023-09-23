1833062f318be814975390eb20370a7836070388
Amar <jeetamar496@gmail.com>
2023-09-23 19:57:18 +0530

    play pause logic written

---
+++ video Player clone javascript/script.js
@@ -0,0 +1,32 @@
+const playPauseBtn= document.querySelector(".play-pause-btn");
+const videoContainer= document.querySelector(".video-container");
+const video=document.querySelector("video");
+
+playPauseBtn.addEventListener("click",toggleplay);
+
+video.addEventListener("click",toggleplay);
+
+document.addEventListener("keydown",e=>{
+switch(e.key.toLowerCase()){
+    case " ":
+    case "k":
+        toggleplay();
+        break;
+}
+
+})
+
+
+
+function toggleplay() {
+    video.paused ? video.play() :video.pause(); 
+}
+
+
+video.addEventListener("play",()=>{
+    videoContainer.classList.remove("paused")
+})
+
+video.addEventListener("pause",()=>{
+    videoContainer.classList.add("paused")
+})
