814cee4639099637efb81b2546b11cce16db437b
Amar <jeetamar496@gmail.com>
2023-09-30 03:14:00 +0530

    coluur flipper done

--- color flipper/app.js
+++ color flipper/app.js
@@ -1,1 +1,18 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn=document.getElementById("btn");
+const colour=document.querySelector(".colour");
+
+btn.addEventListener("click",function () {
+    
+    const randomNumber=getRandomNumber();
+    document.body.style.backgroundColor=colors[randomNumber];
+
+})
+
+
+function getRandomNumber() {
+    console.log(Math.floor(Math.random()*colors.length))
+    return Math.floor(Math.random()*colors.length)
+    
+}
--- color flipper/hex.js
+++ color flipper/hex.js
@@ -1,1 +1,22 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn=document.getElementById("btn");
+const colour=document.querySelector(".color");
+
+btn.addEventListener("click",function () {
+    
+    let hexColor="#"
+    for (let i = 0; i < 6; i++) {
+        hexColor+=hex[getRandomNumber()]  
+    }
+
+    colour.textContent = hexColor;
+    document.body.style.backgroundColor=hexColor;
+
+})
+
+
+function getRandomNumber() {
+    // console.log(Math.floor(Math.random()*colors.length))
+    return Math.floor(Math.random()*hex.length)
+    
+}
