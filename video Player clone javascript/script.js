1a519825d42a9508e2340cb4686ab56e58f64ef1
Amar <jeetamar496@gmail.com>
2023-10-03 00:28:49 +0530

    click issue corrected

--- video Player clone javascript/script.js
+++ video Player clone javascript/script.js
@@ -1,292 +1,230 @@
-const playPauseBtn= document.querySelector(".play-pause-btn");
-const theaterBtn= document.querySelector(".theater-btn");
-const fullScreenBtn= document.querySelector(".full-screen-btn");
-const miniPlayerBtn= document.querySelector(".mini-player-btn");
-const currentTimeElem= document.querySelector(".current-time");
-const totalTimeElem= document.querySelector(".total-time");
-const captionsBtn= document.querySelector(".captions-btn");
-const SpeedBtn= document.querySelector(".speed-btn");
-const previewImg= document.querySelector(".preview-img");
-const thumbnailImg= document.querySelector(".thumbnail-img");
-const muteBtn= document.querySelector(".mute-btn");
-const volumeSlider= document.querySelector(".volume-slider");
-const videoContainer= document.querySelector(".video-container");
-const video=document.querySelector("video");
-const timelineContainer=document.querySelector(".timeline-container");
+const playPauseBtn = document.querySelector(".play-pause-btn")
+const theaterBtn = document.querySelector(".theater-btn")
+const fullScreenBtn = document.querySelector(".full-screen-btn")
+const miniPlayerBtn = document.querySelector(".mini-player-btn")
+const muteBtn = document.querySelector(".mute-btn")
+const captionsBtn = document.querySelector(".captions-btn")
+const speedBtn = document.querySelector(".speed-btn")
+const currentTimeElem = document.querySelector(".current-time")
+const totalTimeElem = document.querySelector(".total-time")
+const previewImg = document.querySelector(".preview-img")
+const thumbnailImg = document.querySelector(".thumbnail-img")
+const volumeSlider = document.querySelector(".volume-slider")
+const videoContainer = document.querySelector(".video-container")
+const timelineContainer = document.querySelector(".timeline-container")
+const video = document.querySelector("video")
 
+document.addEventListener("keydown", e => {
+  const tagName = document.activeElement.tagName.toLowerCase()
 
+  if (tagName === "input") return
 
-
-
-timelineContainer.addEventListener("mousemove",handleTimeUpdate);
-timelineContainer.addEventListener("mousedown",toggleScrubbing);
-
-document.addEventListener("mouseup",(e)=>{
-if(isScrubbing) toggleScrubbing(e);
-
-})
-
-document.addEventListener("mousemove",(e)=>{
-    if(isScrubbing) handleTimeUpdate(e);
-
-})
-
-let isScrubbing=false;
-let wasPaused;
-
-// when we scrub on the video we want this video to show the pasue image in the background
-
-function toggleScrubbing() {
-
-    const rect=timelineContainer.getBoundingClientRect();
-    const percent=Math.min(Math.max(0, e.x - rect.x), rect.width)/rect.width;
-   rect.width
-   isScrubbing = (e.buttons & 1 ) === 1
-   videoContainer.classList.toggle("scrubbing",isScrubbing)
-   if(isScrubbing)
-   {
-    wasPaused=video.paused
-    video.pause()
-   }
-   else{
-    video.currentTime=percent*video.duration
-    if(!wasPaused) {
-        video.play();
-    }
-   }
-
-   handleTimeUpdate(e);
-    
-}
-
-
-
-
-
-
-function handleTimeUpdate(e) {
-    const rect=timelineContainer.getBoundingClientRect();
-    const percent=Math.min(Math.max(0, e.x - rect.x), rect.width)/rect.width;
-    const previewImgNumber=Math.max(1,Math.floor((percent * video.duration)/10));
-    const previewImgSrc=`assests/previewImages/preview${previewImgNumber}.jpg`;
-
-    previewImg.src =previewImgSrc;
-    timelineContainer.style.setProperty("--preview-position",percent);
-
-
-    if(isScrubbing){
-        e.preventDefault();
-        thumbnailImg.src=previewImgSrc;
-        timelineContainer.style.setProperty("--progress-position",percent)
-    }
-    
-}
-
-playPauseBtn.addEventListener("click",toggleplay);
-
-video.addEventListener("click",toggleplay);
-
-document.addEventListener("keydown",e=>{
-    const tagName=document.activeElement.tagName.toLowerCase();
-
-    if(tagName === "input") return;
-
-switch(e.key.toLowerCase()){
+  switch (e.key.toLowerCase()) {
     case " ":
-        if(tagName === "button") return;
+      if (tagName === "button") return
     case "k":
-        toggleplay();
-        break;
-    case "t":
-        toggleTheaterMode();
-        break;
-    case "i":
-        toggleMiniPlayerMode();
-        break;
+      togglePlay()
+      break
     case "f":
-        toggleFullScreenModeff();
-        break;
+      toggleFullScreenMode()
+      break
+    case "t":
+      toggleTheaterMode()
+      break
+    case "i":
+      toggleMiniPlayerMode()
+      break
     case "m":
-            toggleMute();
-            break;
-
-        case "arrowleft":
-            case"j" :
-           skip(-5)
-            break;
-
-         case "arrowright":
-            case"l" :
-           skip(5)
-            break;
-         case "c":
-            toggleCaptions();
-                break;
-
-            
-
-}
-
+      toggleMute()
+      break
+    case "arrowleft":
+    case "j":
+      skip(-5)
+      break
+    case "arrowright":
+    case "l":
+      skip(5)
+      break
+    case "c":
+      toggleCaptions()
+      break
+  }
 })
 
+// Timeline
+timelineContainer.addEventListener("mousemove", handleTimelineUpdate)
+timelineContainer.addEventListener("mousedown", toggleScrubbing)
+document.addEventListener("mouseup", e => {
+  if (isScrubbing) toggleScrubbing(e)
+})
+document.addEventListener("mousemove", e => {
+  if (isScrubbing) handleTimelineUpdate(e)
+})
 
-// speed function
+let isScrubbing = false
+let wasPaused
+function toggleScrubbing(e) {
+  const rect = timelineContainer.getBoundingClientRect()
+  const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width
+  isScrubbing = (e.buttons & 1) === 1
+  videoContainer.classList.toggle("scrubbing", isScrubbing)
+  if (isScrubbing) {
+    wasPaused = video.paused
+    video.pause()
+  } else {
+    video.currentTime = percent * video.duration
+    if (!wasPaused) video.play()
+  }
 
-SpeedBtn.addEventListener("click",changePlaybackSpeed)
+  handleTimelineUpdate(e)
+}
+
+function handleTimelineUpdate(e) {
+  const rect = timelineContainer.getBoundingClientRect()
+  const percent = Math.min(Math.max(0, e.x - rect.x), rect.width) / rect.width
+  const previewImgNumber = Math.max(
+    1,
+    Math.floor((percent * video.duration) / 10)
+  )
+  const previewImgSrc = `assests/previewImages/preview${previewImgNumber}.jpg`
+  previewImg.src = previewImgSrc
+  timelineContainer.style.setProperty("--preview-position", percent)
+
+  if (isScrubbing) {
+    e.preventDefault()
+    thumbnailImg.src = previewImgSrc
+    timelineContainer.style.setProperty("--progress-position", percent)
+  }
+}
+
+// Playback Speed
+speedBtn.addEventListener("click", changePlaybackSpeed)
 
 function changePlaybackSpeed() {
-
-    let newPlaybackRate=video.playbackRate + 0.25;
-    if(newPlaybackRate>2) newPlaybackRate=0.25
-    video.playbackRate =newPlaybackRate
-    SpeedBtn.textContent=`${newPlaybackRate}x`;
-
-    
+  let newPlaybackRate = video.playbackRate + 0.25
+  if (newPlaybackRate > 2) newPlaybackRate = 0.25
+  video.playbackRate = newPlaybackRate
+  speedBtn.textContent = `${newPlaybackRate}x`
 }
 
+// Captions
+const captions = video.textTracks[0]
+captions.mode = "hidden"
 
-// captions
-
-const  captions=video.textTracks[0]
-captions.mode="hidden"
-
-captionsBtn.addEventListener("click",toggleCaptions)
+captionsBtn.addEventListener("click", toggleCaptions)
 
 function toggleCaptions() {
-    const isHidden=captions.mode ==="hidden"
-    captions.mode=isHidden ? "showing" :"hidden"
-    videoContainer.classList.toggle("captions",isHidden)
-    
+  const isHidden = captions.mode === "hidden"
+  captions.mode = isHidden ? "showing" : "hidden"
+  videoContainer.classList.toggle("captions", isHidden)
 }
 
-
-// this shows the complete video duration
-video.addEventListener("loadeddata",()=>{
-    totalTimeElem.textContent=formatDuration(video.duration);
+// Duration
+video.addEventListener("loadeddata", () => {
+  totalTimeElem.textContent = formatDuration(video.duration)
 })
 
-// this will show the current video time
-video.addEventListener("timeupdate",()=>{
-
-    const percent=video.currentTime/video.duration;
-    timelineContainer.style.setProperty("--progress-position",percent)
-    currentTimeElem.textContent=formatDuration(video.currentTime)
+video.addEventListener("timeupdate", () => {
+  currentTimeElem.textContent = formatDuration(video.currentTime)
+  const percent = video.currentTime / video.duration
+  timelineContainer.style.setProperty("--progress-position", percent)
 })
 
-const leadingZeroFormatter=new Intl.NumberFormat(undefined,{
-    minimumIntegerDigits:2,
+const leadingZeroFormatter = new Intl.NumberFormat(undefined, {
+  minimumIntegerDigits: 2,
 })
-
 function formatDuration(time) {
-    const seconds=Math.floor(time%60);
-    const minutes=Math.floor(time/60)%60;
-    const hours=Math.floor(time/3600);
-    if(hours==0)
-    {
-        return `${minutes}:${leadingZeroFormatter.format(seconds)}`
-    }
-
-    else{
-        return `${1}:${leadingZeroFormatter.format(2)}:${leadingZeroFormatter.format(3)}`
-    }
-    
+  const seconds = Math.floor(time % 60)
+  const minutes = Math.floor(time / 60) % 60
+  const hours = Math.floor(time / 3600)
+  if (hours === 0) {
+    return `${minutes}:${leadingZeroFormatter.format(seconds)}`
+  } else {
+    return `${hours}:${leadingZeroFormatter.format(
+      minutes
+    )}:${leadingZeroFormatter.format(seconds)}`
+  }
 }
 
 function skip(duration) {
-    video.currentTime+=duration;
-    
+  video.currentTime += duration
 }
 
-// volume
-
-muteBtn.addEventListener("click",toggleMute);
-volumeSlider.addEventListener("input",e=>{
-    video.volume=e.target.value
-    video.muted = e.target.value === 0;
+// Volume
+muteBtn.addEventListener("click", toggleMute)
+volumeSlider.addEventListener("input", e => {
+  video.volume = e.target.value
+  video.muted = e.target.value === 0
 })
 
 function toggleMute() {
-    video.muted=!video.muted; 
+  video.muted = !video.muted
 }
 
-video.addEventListener("volumechange",()=>{
-    volumeSlider.value=video.volume
-    let volumeLevel
-    if(video.muted || video.volume === 0)
-    {
-        volumeSlider.value=0;
-        volumeLevel= "muted";    
-    }
-    else if(video.volume >=.5)
-    {
-        volumeLevel="high";
-    }
+video.addEventListener("volumechange", () => {
+  volumeSlider.value = video.volume
+  let volumeLevel
+  if (video.muted || video.volume === 0) {
+    volumeSlider.value = 0
+    volumeLevel = "muted"
+  } else if (video.volume >= 0.5) {
+    volumeLevel = "high"
+  } else {
+    volumeLevel = "low"
+  }
 
-    else{   
-        volumeLevel="low"
-    }
-
-    videoContainer.dataset.volumeLevel=volumeLevel;
-  
+  videoContainer.dataset.volumeLevel = volumeLevel
 })
 
-// view modes
-theaterBtn.addEventListener("click",toggleTheaterMode);
-fullScreenBtn.addEventListener("click",toggleFullScreenMode);
-miniPlayerBtn.addEventListener("click",toggleMiniPlayerMode);
-
-
+// View Modes
+theaterBtn.addEventListener("click", toggleTheaterMode)
+fullScreenBtn.addEventListener("click", toggleFullScreenMode)
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode)
 
 function toggleTheaterMode() {
-    videoContainer.classList.toggle("theater")
+  videoContainer.classList.toggle("theater")
 }
 
 function toggleFullScreenMode() {
-  if(document.fullscreenElement ==null)
-  {
-    videoContainer.requestFullscreen();
+  if (document.fullscreenElement == null) {
+    videoContainer.requestFullscreen()
+  } else {
+    document.exitFullscreen()
   }
-  else{
-    document.exitFullscreen();
-  }
-
 }
 
 function toggleMiniPlayerMode() {
-
-    if(videoContainer.classList.contains("mini-player"))
-    {
-        document.exitPictureInPicture();
-
-    }
-    else{
-        video.requestPictureInPicture();
-    }
+  if (videoContainer.classList.contains("mini-player")) {
+    document.exitPictureInPicture()
+  } else {
+    video.requestPictureInPicture()
+  }
 }
 
-video.addEventListener("enterpictureinpicture",()=>{
-    videoContainer.classList.add("mini-player")
+document.addEventListener("fullscreenchange", () => {
+  videoContainer.classList.toggle("full-screen", document.fullscreenElement)
 })
 
-video.addEventListener("leavepictureinpicture",()=>{
-    videoContainer.classList.remove("mini-player")
+video.addEventListener("enterpictureinpicture", () => {
+  videoContainer.classList.add("mini-player")
 })
 
-
-document.addEventListener("fullscreenchange",()=>{
-    // works without ,document.fullscreenElement as well
-    videoContainer.classList.toggle("full-screen",document.fullscreenElement);
+video.addEventListener("leavepictureinpicture", () => {
+  videoContainer.classList.remove("mini-player")
 })
 
-function toggleplay() {
-    video.paused ? video.play() :video.pause(); 
+// Play/Pause
+playPauseBtn.addEventListener("click", togglePlay)
+video.addEventListener("click", togglePlay)
+
+function togglePlay() {
+  video.paused ? video.play() : video.pause()
 }
 
-
-video.addEventListener("play",()=>{
-    videoContainer.classList.remove("paused")
+video.addEventListener("play", () => {
+  videoContainer.classList.remove("paused")
 })
 
-video.addEventListener("pause",()=>{
-    videoContainer.classList.add("paused")
-})  
+video.addEventListener("pause", () => {
+  videoContainer.classList.add("paused")
+})
