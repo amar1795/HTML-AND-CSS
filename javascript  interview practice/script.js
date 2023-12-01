62e9eff3d456173a48e22f452f867b0157534bb0
Amar <jeetamar496@gmail.com>
2023-12-01 23:50:21 +0530

    decorator pattern

--- javascript  interview practice/script.js
+++ javascript  interview practice/script.js
@@ -20,3 +20,29 @@ function debounce(fx, time) {
   };
 }
 
+// decorator pattern
+
+let task = {
+    name: 'demo',
+    heavy(x) {
+      console.log(x + ':heavy:' + this.name);
+      return x + ':heavy' + this.name;
+    },
+  };
+  
+  function memoized(fx) {
+    let map = new Map();
+    return function (x) {
+      if (map.has(x)) {
+        return map.get(x);
+      } else {
+        let memoValue = fx.call(this,x);
+        map.set(x, memoValue);
+        return memoValue;
+      }
+    };
+  }
+  task.memoizedHeavy = memoized(task.heavy) 
+  task.memoizedHeavy(1) // 1:heavydemo
+  
+  
